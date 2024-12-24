385ce05a732672d8314b0360d08c20d8cbf0a1c6
Manuel Apaza <manuel.alfred96@gmail.com>
2024-12-24 17:03:42 -0500

    Agregando cambios 5

--- src/routers/routes.jsx
+++ src/routers/routes.jsx
@@ -6,12 +6,11 @@ export function MyRoutes() {
     return(
         <Router>
             <Routes>
-                <Route exact path="/" element={<LoginPage/>} />
+                <Route index element={<LoginPage/>} />
                 <Route path="/dashboard/" element={<DashboardPage />}>
                     <Route index element={<ContextMovieCard />} />
                     <Route path="moviedetails/:movieId" element={<MovieDetails />} />
                 </Route>
-                <Route path="*" element={<LoginPage />}/>
             </Routes>
         </Router>
     )
