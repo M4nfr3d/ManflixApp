10bf21551fdd07f6bf9a239081e10d51dea8d62d
Manuel Apaza <manuel.alfred96@gmail.com>
2024-12-24 08:13:18 -0500

    Importando normalize.css

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import './App.css'
 import { FooterComponent } from './components'
 import { MyRoutes } from './routers/routes'
 import { LoginProvider } from './util/LoginProvider'
+import './normalize.css'
 
 
 function App() {
--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { LoginProvider } from './util/LoginProvider'
 import App from './App'
-import './normalize.css'
+// import './normalize.css'
 
  createRoot(document.getElementById('root')).render(
   <StrictMode>
