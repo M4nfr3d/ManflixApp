3a0ffbab3b4460ffc04c030d0098b88811614add
Manuel Apaza <manuel.alfred96@gmail.com>
2024-12-24 10:02:31 -0500

    Agregando cambios

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,6 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { LoginProvider } from './util/LoginProvider'
 import App from './App'
-// import './normalize.css'
 
  createRoot(document.getElementById('root')).render(
   <StrictMode>
